d8b1c7f46a0c757c5157b88a942a18dbcfb6c371
JCwynar <101180601+167689@users.noreply.github.com>
2022-06-21 05:51:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Int wariant;
+    
+        System.out.println("Witaj w programie przeliczającym kody liczbowe");
+
+    System.out.println("Wybierz jaki kod chcesz wprowadzić ");
+     
+    System.out.println("1 Dwójkowy (BIN)");
+    System.out.println("2  Ósemkowy (OCT)");
+    System.out.println("3  Dziesiętny (DEC)");
+    System.out.println("4  Szesnastkoy (HEX)");
+     Scanner scan = new Scanner(System.in);
+    Int a =scan.nextInt();
+    a=wariant;
+    System.out.println("Wybierz na jaki kod chcesz przeliczyć");
+     System.out.println("1 Dwójkowy (BIN)");
+    System.out.println("2  Ósemkowy (OCT)");
+    System.out.println("3  Dziesiętny (DEC)");
+    System.out.println("4  Szesnastkoy (HEX)");
+    Int b =scan.nextInt();
+     
+    
+     
+    
+    switch(wariant){
+      case 1:
+         System.out.println("Podaj kod");
+    Int kod =scan.nextInt();
+        
+        break;
+        case 2:
+         System.out.println("Podaj kod");
+    Int kod =scan.nextInt();
+        
+        break;
+      case 3:
+         System.out.println("Podaj kod");
+    Int kod =scan.nextInt();
+        
+        break;
+      case 4:
+         System.out.println("Podaj kod");
+    Int kod =scan.nextInt();
+        
+        break;
+        
